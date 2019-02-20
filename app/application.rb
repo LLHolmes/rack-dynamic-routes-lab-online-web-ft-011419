92ac5f76800b5202ee75d3a6c87cf0dc597f59a1
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-20 15:44:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = Item.find_by_name(item_name)
-      if item.name
+      if item
         resp.write "#{item.name} costs $#{item.price}"
       else
         resp.write "Item not found"
