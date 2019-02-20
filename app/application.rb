cf273e6011e9a5565eb4ac393898fae11dcd78e3
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-20 15:25:37 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path=="/songs"
+    resp.write "You requested the songs"
+  else
+    resp.write "Route not found"
+    resp.status = 404				# Sets the status code.
+  end
+
+  resp.finish
+end
+end
