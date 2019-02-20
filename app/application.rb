1290f28f92fce9b687a23d035b9944b4b3583c06
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-20 15:36:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,12 @@
 class Application
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      requested_item = req.path.split("/items/").last
-      song = @@songs.find{|s| s.title == song_title}
+      item_name = req.path.split("/items/").last
+      item = @@songs.find{|s| s.title == song_title}
       
       resp.write song.artist
     end
