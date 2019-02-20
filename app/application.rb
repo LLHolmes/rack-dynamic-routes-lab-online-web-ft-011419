fb5aef37a52782426ba536d6164b936dfc14d392
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-20 15:25:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,15 @@
 class Application
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-  if req.path=="/songs"
-    resp.write "You requested the songs"
-  else
-    resp.write "Route not found"
-    resp.status = 404				# Sets the status code.
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404				# Sets the status code.
+    end
+
+    resp.finish
   end
-
-  resp.finish
-end
 end
